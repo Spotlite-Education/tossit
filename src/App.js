5e9ee8cdf1325d61dad130c9abd7d403d14c2dbb
soap <queazzyy@gmail.com>
2021-12-10 22:27:49 -0800

    ADD: Rapidly Rotating Andrews

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import logo from './kk.png';
+import logo2 from './andrew.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          andrew dog.
+          don't learn react
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className=".App-logo">
+          <img src={logo} className = "App-logo" alt="logo"/>
+        </div>
       </header>
+
     </div>
   );
 }
