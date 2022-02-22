a470cc16e30052eacb1727b107f17924d0dca9ba
Yuchen Xin <xin.yuchen@hotmail.com>
2022-02-22 00:11:05 -0800

    Add: socket-io client integration, code room joining
    TODO (for Andrew): complete both TODO comments in file src/routes/Home.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Home from './routes/Home';
 import './App.scss';
+import { io } from 'socket.io-client';
+
+const serverURL = "http://localhost:8000"; // TODO: change to heroku app URL later
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(serverURL);
+    setSocket(newSocket);
+
+    newSocket.on('debugMessage', message => {
+      console.log(message);
+    });
+
+    return () => newSocket.close();
+  }, [setSocket]);
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home socket={socket} />} />
         <Route path="about" element={<About />} />
       </Routes>
     </div>
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../components/input';
 import '../styles/Home.scss';
 
-function Home() {
-    const [loading, setLoading] = React.useState(false);
-    const handleSubmit = (code) => {
-        console.log(code);
+const Home = ({ socket }) => {
+    const [loading, setLoading] = useState(false);
+    const [code, setCode] = useState("");
+    const [username, setUsername] = useState("amogus"); // TMP
+
+    const handleJoin = () => {
+        console.log("Attempting to join with code " + code);
         setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
+
+        // TODO change query - follow this format: toss.it/?code=${code}
+        socket.emit('joinRoom', {
+            code,
+            username,
+        });
+
+        setLoading(false);
     }
+
     return (
       <>
         <main>
             <h1 id='title'>TOSS - IT</h1>
-            <Input width='22.5%' height='3.5rem' numInputs={6} outlineStyle='underscore' onSubmit={(code) => handleSubmit(code)} />
+            <Input width='22.5%' height='3.5rem' numInputs={6} outlineStyle='underscore' onSubmit={(newCode) => setCode(newCode)} />
+
+            {/* TODO: either add a second input here for username, or link/route to another webpage to input name. When both
+            code and username states are set, then call handleJoin(). */}
+            <button onClick={handleJoin}>Enter</button> {/* TMP */}
         </main>
-        {loading && 
+        {loading &&
             <div id='loading'>
                 <p id='loading-text'>Validating Code...</p>
             </div>
