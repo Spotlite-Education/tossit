30dc89cfefe9fdf5e1292fed15854a402ec441ac
austinhong <nistuagnoh@gmail.com>
2022-03-09 23:39:47 -0800

    Add: timer and header
    Added a timer function, needs stylizing. Suggestion: make the time variable and changed by the sender as a dropdown menu.

--- src/components/AnswerBoard.js
+++ src/components/AnswerBoard.js
@@ -2,11 +2,33 @@ import React from 'react';
 
 const AnswerBoard = ({question, answerChoices, onSubmit}) => {
     
+    const studentQuestion = question;
+    const choices = answerChoices.slice();
+
     const [finalAnswer,setAnswer] = React.useState(null);
     const [sendable,setSendable] = React.useState(false);
 
-    const studentQuestion = question;
-    const choices = answerChoices.slice();
+    const [minutes, setMinutes] = React.useState(0);
+    const [seconds, setSeconds] = React.useState(0);
+
+    React.useEffect(() => {
+
+        //default is 5 minutes
+        const [time, setTime] = 300000;
+
+        const interval = setInterval(() => {
+            setTime(time-1000);
+
+            const m = Math.floor((time) / (1000 * 60));
+            setMinutes(m);
+
+            const s = Math.floor((time % (1000*60)) / (1000));
+            setSeconds(s);
+
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, []);
 
     const saveAnswer = (answer) => {
         setAnswer(answer);
@@ -25,8 +47,21 @@ const AnswerBoard = ({question, answerChoices, onSubmit}) => {
     return(
         <div className="Question">
             {/* Header */}
+            <div className = "stylized-headers">
+                <h1>Answer the tossed question!</h1>
+            </div>
 
             {/* Timer */}
+            <div className = "timer">
+                <div className = "timer-segment">
+                    <span className = "time">{minutes}</span>
+                    <span className = "label">Minutes</span>
+                </div>
+                <div className = "timer-segment">
+                    <span className = "time">{seconds}</span>
+                    <span className = "label">Seconds</span>
+                </div>
+            </div>
 
             {/* Question */}
             <div className = "stylized-answering">
@@ -40,7 +75,6 @@ const AnswerBoard = ({question, answerChoices, onSubmit}) => {
             </div>
 
             {/* Save/Submit Answer Buttons */}
-
             <div className = "stylized-buttons">
                 <button onClick={saveAnswer}>Save</button>
                 <button onClick={sendAnswer}>Submit</button>
