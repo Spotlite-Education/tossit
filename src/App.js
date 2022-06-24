d78f0a010406e04e1b80e65bd48532101a89b2f1
Andrew Zhou <andrewzhou16@gmail.com>
2022-06-24 16:01:18 -0700

    Fix: wrap app with router component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { SocketContext, socket } from './context/socket';
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss'; 
 import AnimatedRoutes from './AnimatedRoutes';
 
 function App() {
   return (
     <SocketContext.Provider value={socket}>
-      <AnimatedRoutes />
+      <BrowserRouter>
+        <AnimatedRoutes />
+      </BrowserRouter>
     </SocketContext.Provider>
   );
 }
