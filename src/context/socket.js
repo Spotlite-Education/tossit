c6beb2a12eabf0e447ebc123af606d69062b17e2
Andrew Zhou <andrewzhou16@gmail.com>
2022-06-24 15:22:06 -0700

    Fix: socket io context

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,10 +1,9 @@
-import socketio from 'socket.io-client';
+import { io } from 'socket.io-client';
 import React from 'react';
 
-const SOCKET_URL = 'https://tossit-server.herokuapp.com/';
-//const SOCKET_URL = 'http://localhost:8000';
+const SOCKET_URL = 'https://tossit-server.herokuapp.com';
 
-const socket = socketio.connect(SOCKET_URL);
+const socket = io(SOCKET_URL);
 socket.on('debugMessage', (props) => console.log("DEBUG: " + props.message));
 socket.on('errorMessage', (props) => console.log("ERROR: " + props.message)); // TODO: socket.off these and allListeners in the end
 export { socket };
