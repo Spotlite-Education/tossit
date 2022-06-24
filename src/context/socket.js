7706da0e95575e8f99a069b3077a4c0a2f73fae7
Andrew Zhou <andrewzhou16@gmail.com>
2022-06-24 12:46:23 -0700

    Fix: server wrong url

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,7 +1,7 @@
 import socketio from 'socket.io-client';
 import React from 'react';
 
-const SOCKET_URL = 'http://localhost:8000'; //'https://tossit-server.herokuapp.com/';
+const SOCKET_URL = 'https://tossit-server.herokuapp.com/'; //'https://tossit-server.herokuapp.com/';
 
 const socket = socketio.connect(SOCKET_URL);
 socket.on('debugMessage', (props) => console.log("DEBUG: " + props.message));
