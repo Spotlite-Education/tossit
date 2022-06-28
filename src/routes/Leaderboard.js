d8514f4d3aca7a60a8a9239ba61f059eeebde07d
soap <queazzyy@gmail.com>
2022-06-27 22:39:55 -0700

    Some leaderboard styling
    WHYYYYY IS CSS SO HARD

--- src/routes/Leaderboard.js
+++ src/routes/Leaderboard.js
@@ -5,27 +5,30 @@ import { SocketContext } from '../context/socket';
 import Corner from '../components/Corner';
 import PlayerWorkBox from '../components/PlayerWorkBox';
 import '../App.scss';
+import plane from '../assets/images/plane.svg';
 
 const PlayerResult = ({ data }) => {
     const [showToss, setShowToss] = React.useState(false);
 
     let placeColor;
     if (data.place === 1) {
-        placeColor = 'rgb(255, 215, 0)';
+        placeColor = 'invert(3%) sepia(26%) saturate(4505%) hue-rotate(309deg) brightness(115%) contrast(107%)';
     } else if (data.place === 2) {
-        placeColor = 'rgb(220, 220, 220)';
+        placeColor = 'invert(10%) sepia(26%) saturate(100%) hue-rotate(87deg) brightness(100%) contrast(93%)';
     } else if (data.place === 3) {
-        placeColor = 'rgb(205, 127, 50)';
+        placeColor = 'invert(33%) sepia(31%) saturate(391%) hue-rotate(1deg) brightness(91%) contrast(83%)';
     } else {
-        placeColor = 'white';
+        placeColor = 'brightness(90%)';
     }
+
     return (
         <button className='leaderboard-result' onClick={() => setShowToss(!showToss)}>
-            <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
-                <p style={{ flex: 1, textAlign: 'left', color: placeColor }}>{data.place}.</p>
-                <b style={{ flex: 4, textAlign: 'center' }}>{data.username}</b>
-                <p style={{ flex: 1, textAlign: 'right', color: '#DFDFDF' }}>{data.score}</p>
+            <div style={{ display: 'flex', flexDirection: 'row', width: '100%',}}>
+                <img src={plane} style={{verticalAlign: 'middle', width: '50px', flex: 1, textAlign: 'left', filter: placeColor}} alt="plane"></img>
+                <div style={{ flex: 4, verticalAlign: 'middle', textAlign: 'center', color: '#9C9C9C' }}>{data.username}</div>
+                <div style={{ flex: 1, verticalAlign: 'middle', textAlign: 'right', color: '#CCCCCC ' }}>{data.score}</div>
             </div>
+
             {showToss && <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <PlayerWorkBox
                     username={data.username}
